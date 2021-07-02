03350b823908742b7410ebdcc24536e1f855a6a6
CuJIbBecTp <fiodar.hancharou@inria.fr>
2021-07-02 23:46:08 +0200

    almost all requests are working

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask, request
+from flask import Flask
+import requests
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
+import io
+
+# Download the list of Dealers
+makes_url = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=csv'
+makes_string = requests.get(makes_url).text
+makes = io.StringIO(makes_string)
+makes_pd = pd.read_csv(makes, sep=",")
+makes = [i.lower() for i in (makes_pd.iloc[:,1]).values.tolist()]
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,16 +30,13 @@ class CarModel(db.Model):
 
 
 car_put_args = reqparse.RequestParser()
-car_put_args.add_argument("make", type=str, help="Manufacturer of the car", required=True)
+car_put_args.add_argument("make", type=str, help="Make of the car", required=True)
 car_put_args.add_argument("model", type=str, help="Model of the car", required=True)
-car_put_args.add_argument("rate", type=float, help="Rating of the car", default=0.0)
-
-car_post_args = reqparse.RequestParser()
-car_post_args.add_argument("make", type=str, help="Make of the car", required=True)
-car_post_args.add_argument("model", type=str, help="Model of the car", required=True)
+car_put_args.add_argument("rate", type=float, help="Rating of the car", default=1.0)
 
 car_update_args = reqparse.RequestParser()
-car_update_args.add_argument("rate", type=float, help="Rating of the car", required=False)
+car_update_args.add_argument("car_id", type=int, help="id of the car", required=True)
+car_update_args.add_argument("rating", type=float, help="Rating of the car in the range [1,5]", required=True)
 
 resource_fields = {
     'id': fields.String,
@@ -36,6 +44,52 @@ resource_fields = {
     'model': fields.String,
     'rate': fields.Float
 }
+
+
+@api.resource("/cars","/cars/")
+class Cars(Resource):
+    @marshal_with(resource_fields)
+    def post(self):
+        args = car_put_args.parse_args()
+        make = args['make'].lower()
+        if make not in makes:
+            abort(404, message="Make doesn't exist")
+        model_url = f'https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMake/{make}?format=csv'
+        model_string = requests.get(model_url).text
+        model = io.StringIO(model_string)
+        model_pd = pd.read_csv(model, sep=",")
+        models = [i.lower() for i in (model_pd.iloc[:,3]).values.tolist()]
+        model = args['model'].lower()
+        if model not in models:
+            abort(404, message="Model doesn't exist")
+        car = CarModel(make=make.capitalize(), model=model.capitalize(), rate=args['rate'])
+        db.session.add(car)
+        db.session.commit()
+        return car, 201
+
+    @marshal_with(resource_fields)
+    def get(self):
+        result = CarModel.query.all()
+        if not result:
+            abort(404, message=f'The database is empty')
+        print(result)
+        return result
+
+
+@api.resource("/rate","/rate/")
+class CarsRating(Resource):
+    @marshal_with(resource_fields)
+    def post(self):
+        args = car_update_args.parse_args()
+        result = CarModel.query.filter_by(id=args['car_id']).first()
+        if not result:
+            abort(404, message="Car doesn't exist, cannot update")
+        if 1 <= args['rating'] <= 5:
+            result.rate = args['rating']
+        else:
+            abort(404, message="Value does not satisfy the range [1,5]")
+        db.session.commit()
+        return result
 
 
 @api.resource("/cars/<int:car_id>")
@@ -74,8 +128,8 @@ class Car(Resource):
         result = CarModel.query.filter_by(id=car_id).first()
         if not result:
             abort(404, message="Car doesn't exist, cannot update")
-        if args['rate']:
-            result.rate = args['rate']
+        if args['rating']:
+            result.rate = args['rating']
         db.session.commit()
         return result
 
--- test.py
+++ test.py
@@ -4,22 +4,39 @@ BASE = "http://127.0.0.1:5000/"
 
 data = [{"make": "volkswagen", "model": "passat", "rate": 3},
         {"make": "BMW", "model": "s5", "rate": 4},
-        {"make": "mercedes", "model": "E220", "rate": 5},
+        {"make": "mercedes", "model": "E220", "rate": 1},
         {"make": "renault", "model": "scenic"}]
 
+print('Add 4 rows to the DB')
 for i in range(len(data)):
     response = requests.put(BASE + "cars/" + str(i), data[i])
     print(response.json())
+
+print('Add 1 row using POST and models check')
+response = requests.post(BASE + "cars/", {"make": "Volkswagen", "model": "Golf"})
+print(response.json())
 
 input()
 response = requests.get(BASE + "cars/2")
 print(response.json())
 
+# test of rate update
 input()
-response = requests.patch(BASE + "cars/2", {"rate": 4.55})
+print('Test of the rating update')
+response = requests.post(BASE + "rate/", {"car_id": 1, "rating": 5})
+print(response.json())
+response = requests.post(BASE + "rate", {"car_id": 1, "rating": 5})
 print(response.json())
 
 input()
+print('Everything that database has:')
+response = requests.get(BASE + "cars/")
+print(response.json())
+print('Everything that database has:')
+response = requests.get(BASE + "cars")
+print(response.json())
+
+print('Deletion of all database')
 for i in range(len(data)):
     response = requests.delete(BASE + "cars/" + str(i))
 print(response.json())
